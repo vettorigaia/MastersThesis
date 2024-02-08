12c95aea3c27f5a00926d4e19166c6a9c8b095f4
vettorigaia <gaia.vettori@mail.polimi.it>
2024-02-08 18:21:51 +0100

    Update NewLibraryENGcopia.py

--- NewLibraryENGcopia.py
+++ NewLibraryENGcopia.py
@@ -130,20 +130,22 @@ def poiproc(file,target,stim,):
     for neuron in tqdm(list_neurons):
         neuron=neuron[neuron>0*10000]
         neuron=neuron[neuron<200*10000]
-        print('  Neuron with ',neuron.shape[0],'spikes')
+        #print('  Neuron with ',neuron.shape[0],'spikes')
         if neuron.shape[0]>1000:
             
-            counter+=1
+            print('  Neuron with ',neuron.shape[0],'spikes')
         else:
             print('    Excluded neuron with n spikes = ',neuron.shape[0])
             continue
         
         ISI_healthy = np.diff(neuron)/10000
         map_estimate = Bayesian_mixture_model(ISI_healthy)
-        map_estimate['Target']=target
-        map_estimate['Stimulation']=stim
-        df = pd.DataFrame.from_dict(map_estimate,orient='index')
-        dataframe = pd.concat([dataframe,df],axis = 1)
+        if map_estimate!=0:
+            counter+=1
+            map_estimate['Target']=target
+            map_estimate['Stimulation']=stim
+            df = pd.DataFrame.from_dict(map_estimate,orient='index')
+            dataframe = pd.concat([dataframe,df],axis = 1)
     print('Final number of neurons: ',counter)
     print('Target = ',target)
     file_name = file.split("/")[-1]
@@ -179,73 +181,7 @@ def cut_all(all,data):
     firing_rate=len(all_new)*10000/len(data)
     print(len(all)-len(all_new),' spikes removed;  ', 'firing rate: {:.2f}'.format(firing_rate),'Hz')
     return cut,all_new
-
-def new_cut(all,data):
-    pre = 0.0015
-    post = 0.0015
-    fs=10000
-    prima = int(pre*fs)
-    dopo = int(post*fs)
-    lunghezza_indici = len(all)
-    cut= np.empty([lunghezza_indici, prima+dopo])
-    dim = data.shape[0]
-    k=0
-    coeff=1.5
-    signal_std=np.std(data)
-    standard_threshold=signal_std
-    for i in all:
-        if (i-prima >= 0) and (i+dopo <= dim):
-            spike= data[(int(i)-prima):(int(i)+dopo)].squeeze()
-            cut[k,:] = spike
-            k+=1
-    standards=np.std(cut)
-    indices_to_keep=np.all(standards<coeff*standard_threshold,axis=1)
-    all_new=indices_to_keep
-    filtered_cut=cut[indices_to_keep]
-    k=0
-    cut=np.empty([indices_to_keep, prima+dopo])
-    for spike in filtered_cut:
-        media=(np.mean(spike))
-        std=np.std(spike)
-        spike_std=(spike-media)/std
-        cut[k,:]=spike_std
-        k+=1
-    firing_rate=len(all_new)*10000/len(data)
-    print(len(all)-len(all_new),' spikes removed;  ', 'firing rate: {:.2f}'.format(firing_rate),'Hz')
-    return cut,all_new
-    
-
-
-def find_all_spikes(data,thresh):
-    spike_length=30 #3ms
-    ind, peaks_amp = scipy.signal.find_peaks(abs(data), height=thresh, distance= spike_length)
-    firing_rate=(len(ind)*10000)/len(data)
-    #print(len(ind), ' spikes detected;  ', 'firing rate: {:.2f}'.format(firing_rate),'Hz')
-    return ind
-
-def windowed_spike_detection(data):
-    spike_length=30 #3ms
-    window_length=10000 #1 sec
-    abs_data=abs(data)
-    i=0
-    ind=[]
-    while i < len(data)+window_length:
-        abs_window=abs_data[i:i+window_length]
-        window=data[i:i+window_length]
-        #if abso==0:
-            #thresh=coeff*(scipy.stats.median_abs_deviation(window,scale='normal'))
-        #else:
-        coeff=4
-        thresh=coeff*(scipy.stats.median_abs_deviation(abs_window,scale='normal'))
-        ind1, peaks =find_peaks(abs_window, height=thresh,distance=spike_length)
-        last=i
-        if len(ind1):
-            last=i+ind1[-1]
-        ind.extend([index + i for index in ind1])
-        i=last+spike_length
-    firing_rate=len(ind)*10000/len(data)
-    print(len(ind), ' spikes detected;  ', 'firing rate: {:.2f}'.format(firing_rate),'Hz')
-    return ind
+ 
 def spike_detection(data):
     spike_length=30 #3ms (0.003s)
     window_length=1000 #0.1 sec (100ms)
@@ -275,94 +211,13 @@ def spike_detection(data):
     print(len(ind), ' spikes detected;  ', 'firing rate: {:.2f}'.format(firing_rate),'Hz')
     return ind
 
-def select_clus(cut,spike_list,data):
-    from sklearn.cluster import KMeans
-    from sklearn.preprocessing import StandardScaler
-    from sklearn.decomposition import PCA
-    from sklearn.metrics import silhouette_score
-    import numpy as np
-    scale = StandardScaler()
-    estratti_norm = scale.fit_transform(cut)
-    print('\n______________________________________________________________________________________________________________')
-    print('Total spikes: ', estratti_norm.shape[0])
-    n_comp=3
-    pca = PCA(n_components=n_comp)
-    transformed = pca.fit_transform(estratti_norm)
-    spike_list=np.array(spike_list)
-    kmeans_score=[]
-    final_data=[]
-    for n in range (2,4):
-        model = KMeans(n_clusters=n, init='k-means++', n_init=10, max_iter=400, tol=0.25, verbose=0, random_state=None, copy_x=True,  algorithm='lloyd')
-        labels = model.fit_predict(transformed)
-        silhouette_avg = silhouette_score(transformed, labels)
-        kmeans_score.append(silhouette_avg)
-    top_clusters_kmeans = kmeans_score.index(max(kmeans_score))+2
-    if max(kmeans_score)>=0.4:
-        print("\n\n\033[1;31;47mBest cluster in the range 1 to 3: ",top_clusters_kmeans,", with a silhouette score of: ",max(kmeans_score), "\u001b[0m  \n\n")
-        model = KMeans(n_clusters=top_clusters_kmeans,  init='k-means++', n_init=10, max_iter=400, tol=0.25, verbose=0, random_state=None, copy_x=True,  algorithm='lloyd')
-        labels = model.fit_predict(transformed)
-    else:
-        print('Clustering algorithm detected only one cluster')
-        labels=np.zeros(len(spike_list),dtype=int)
-    unique_labels=np.unique(labels)
-    firings=np.zeros(len(unique_labels))
-    color=[]
-    for i in labels:
-        color.append(plt.rcParams['axes.prop_cycle'].by_key()['color'][i])
-    for i,cluster_label in enumerate(unique_labels):
-        cluster_data=cut[labels==cluster_label]
-        mean_wave=np.mean(cluster_data, axis=0)
-        std_wave=np.std(cluster_data, axis=0)
-        distances=np.abs(cluster_data - mean_wave)
-        distance_threshold=2*std_wave
-        indices_to_keep=np.all(distances<=distance_threshold,axis=1)
-        filtered_cluster_data=cluster_data[indices_to_keep]
-        plotting_data=filtered_cluster_data.transpose()
-        firings[i]=len(filtered_cluster_data)*10000/len(data)
-        plt.subplot(3,1,i+1)
-        plt.plot(plotting_data,alpha=0.5)
-        plt.title(f'Cluster {i} \n numerosity: {len(filtered_cluster_data)}')
-        plt.xlabel('Time [ms]')
-        plt.ylabel('Signal Amplitude')
-        mean_wave = np.mean(filtered_cluster_data, axis=0)
-        std_wave = np.std(filtered_cluster_data, axis=0)
-        plt.errorbar(range(mean_wave.shape[0]), mean_wave, yerr=std_wave, color='black', linewidth=2, label='Avg. Waveform')
-        plt.legend(loc='lower right')
-        plt.show()
-        ul=spike_list[labels==i]
-        ull=ul[indices_to_keep]
-        final_data.append(ull)
-        plt.subplot(3, 1, i + 1)
-        plt.hist(np.diff(ull), bins=100, density=True, alpha=0.5, color='blue', edgecolor='black')
-        plt.title(f'ISI: Cluster {i}, \n firing rate: {format(len(final_data[i])*10000/len(data), ".2f")} Hz')
-        plt.show()
-    if len(unique_labels)>10:
-        fig = plt.figure(figsize=(18,8))
-        ax = fig.add_subplot(1, 2, 1, projection='3d')
-        ax.scatter(transformed[:,0], transformed[:,1], transformed[:,2], c=color, alpha=0.8, s=10, marker='.')
-        ax = fig.add_subplot(1, 2, 2)
-        for i,cluster_label in enumerate(unique_labels):    
-            idx=cluster_label==i
-            color = plt.rcParams['axes.prop_cycle'].by_key()['color'][i]
-            mean_wave = np.mean(cut[idx,:],axis = 0)
-            std_wave = np.std(cut[idx,:],axis = 0)
-            #ax.errorbar(range(cut[idx,:].shape[1]),mean_wave,yerr = std_wave)
-            plt.errorbar(range(mean_wave.shape[0]), mean_wave, yerr=std_wave, c=color)
-            #ax.errorbar(range(mean_wave.shape[0]),mean_wave,yerr = std_wave)
-
-        plt.xlabel('Time [0.1ms]')
-        plt.ylabel('Voltage [\u03BCV]')
-        plt.show()    
-
-    return final_data
-
 ################################POINT PROCESS
 def Bayesian_mixture_model(ISI_data):
     import scipy.stats as st
     with pm.Model() as model:
         ##### WALD DISTRIBUTION (INVERSE GAUSSIAN)
         mu1 = pm.Uniform('mu1',lower=0.001,upper=0.02)
-        lam1 = pm.Uniform('lam1',lower=0.001,upper=0.04)
+        lam1 = pm.Uniform('lam1',lower=0.001,upper=0.02)
         obs1 = pm.Wald.dist(mu=mu1,lam=lam1)
 
 
@@ -376,20 +231,6 @@ def Bayesian_mixture_model(ISI_data):
         
         w = pm.Dirichlet('w', a=np.array([1., 1., 1.]))
         like = pm.Mixture('like', w=w, comp_dists = [obs1, obs2, obs3], observed=ISI_data)
-
-        '''
-        if counter==9:
-            step = pm.NUTS(target_accept=0.9)
-            trace = pm.sample(step=step,draws=1000,chains=1,tune=1000,cores=4)
-            ppc_trace = pm.sample_posterior_predictive(trace,model=model)
-            bins = np.arange(0, .5, 1e-3) 
-            plt.figure (figsize=(14,10))
-            hist = np.histogram(ppc_trace['posterior_predictive']['like'].values,bins=bins)
-            #plt.axis([-0.01,0.13,0,160])
-            a= plt.hist(ISI_data,bins)
-            plt.plot(hist[1][:-1],hist[0]/1000,linewidth=3)
-            plt.show()
-        '''
         
     map_estimate = pm.find_MAP(model=model)
     d= np.linspace(0.00, 1, len(ISI_data))
@@ -430,6 +271,8 @@ def Bayesian_mixture_model(ISI_data):
     del map_estimate['w']
 
     print(map_estimate)
+    if ks_score[0]>0.3:
+        map_estimate=0
     return map_estimate
 
 def butter_bandpass(lowcut, highcut, fs, order=5):
@@ -522,6 +365,71 @@ def clus(cut,spike_list,data):
 
 
 '''
+def new_cut(all,data):
+    pre = 0.0015
+    post = 0.0015
+    fs=10000
+    prima = int(pre*fs)
+    dopo = int(post*fs)
+    lunghezza_indici = len(all)
+    cut= np.empty([lunghezza_indici, prima+dopo])
+    dim = data.shape[0]
+    k=0
+    coeff=1.5
+    signal_std=np.std(data)
+    standard_threshold=signal_std
+    for i in all:
+        if (i-prima >= 0) and (i+dopo <= dim):
+            spike= data[(int(i)-prima):(int(i)+dopo)].squeeze()
+            cut[k,:] = spike
+            k+=1
+    standards=np.std(cut)
+    indices_to_keep=np.all(standards<coeff*standard_threshold,axis=1)
+    all_new=indices_to_keep
+    filtered_cut=cut[indices_to_keep]
+    k=0
+    cut=np.empty([indices_to_keep, prima+dopo])
+    for spike in filtered_cut:
+        media=(np.mean(spike))
+        std=np.std(spike)
+        spike_std=(spike-media)/std
+        cut[k,:]=spike_std
+        k+=1
+    firing_rate=len(all_new)*10000/len(data)
+    print(len(all)-len(all_new),' spikes removed;  ', 'firing rate: {:.2f}'.format(firing_rate),'Hz')
+    return cut,all_new
+
+def find_all_spikes(data,thresh):
+    spike_length=30 #3ms
+    ind, peaks_amp = scipy.signal.find_peaks(abs(data), height=thresh, distance= spike_length)
+    firing_rate=(len(ind)*10000)/len(data)
+    #print(len(ind), ' spikes detected;  ', 'firing rate: {:.2f}'.format(firing_rate),'Hz')
+    return ind
+
+def windowed_spike_detection(data):
+    spike_length=30 #3ms
+    window_length=10000 #1 sec
+    abs_data=abs(data)
+    i=0
+    ind=[]
+    while i < len(data)+window_length:
+        abs_window=abs_data[i:i+window_length]
+        window=data[i:i+window_length]
+        #if abso==0:
+            #thresh=coeff*(scipy.stats.median_abs_deviation(window,scale='normal'))
+        #else:
+        coeff=4
+        thresh=coeff*(scipy.stats.median_abs_deviation(abs_window,scale='normal'))
+        ind1, peaks =find_peaks(abs_window, height=thresh,distance=spike_length)
+        last=i
+        if len(ind1):
+            last=i+ind1[-1]
+        ind.extend([index + i for index in ind1])
+        i=last+spike_length
+    firing_rate=len(ind)*10000/len(data)
+    print(len(ind), ' spikes detected;  ', 'firing rate: {:.2f}'.format(firing_rate),'Hz')
+    return ind
+
 def neg_RMM(data):
     # window size 100ms, threshold for first spike: 4*mad(window), threshold for second spike: 1.1 mean(window)
     # differential threshold: 7*mad(window)
@@ -654,4 +562,85 @@ def RMM(data):
     firing_rate=len(minima)*10000/len(data)
     print('detected spikes:', len(minima), 'firing rate: ',firing_rate)
     return minima, maxima
+def select_clus(cut,spike_list,data):
+    from sklearn.cluster import KMeans
+    from sklearn.preprocessing import StandardScaler
+    from sklearn.decomposition import PCA
+    from sklearn.metrics import silhouette_score
+    import numpy as np
+    scale = StandardScaler()
+    estratti_norm = scale.fit_transform(cut)
+    print('\n______________________________________________________________________________________________________________')
+    print('Total spikes: ', estratti_norm.shape[0])
+    n_comp=3
+    pca = PCA(n_components=n_comp)
+    transformed = pca.fit_transform(estratti_norm)
+    spike_list=np.array(spike_list)
+    kmeans_score=[]
+    final_data=[]
+    for n in range (2,4):
+        model = KMeans(n_clusters=n, init='k-means++', n_init=10, max_iter=400, tol=0.25, verbose=0, random_state=None, copy_x=True,  algorithm='lloyd')
+        labels = model.fit_predict(transformed)
+        silhouette_avg = silhouette_score(transformed, labels)
+        kmeans_score.append(silhouette_avg)
+    top_clusters_kmeans = kmeans_score.index(max(kmeans_score))+2
+    if max(kmeans_score)>=0.4:
+        print("\n\n\033[1;31;47mBest cluster in the range 1 to 3: ",top_clusters_kmeans,", with a silhouette score of: ",max(kmeans_score), "\u001b[0m  \n\n")
+        model = KMeans(n_clusters=top_clusters_kmeans,  init='k-means++', n_init=10, max_iter=400, tol=0.25, verbose=0, random_state=None, copy_x=True,  algorithm='lloyd')
+        labels = model.fit_predict(transformed)
+    else:
+        print('Clustering algorithm detected only one cluster')
+        labels=np.zeros(len(spike_list),dtype=int)
+    unique_labels=np.unique(labels)
+    firings=np.zeros(len(unique_labels))
+    color=[]
+    for i in labels:
+        color.append(plt.rcParams['axes.prop_cycle'].by_key()['color'][i])
+    for i,cluster_label in enumerate(unique_labels):
+        cluster_data=cut[labels==cluster_label]
+        mean_wave=np.mean(cluster_data, axis=0)
+        std_wave=np.std(cluster_data, axis=0)
+        distances=np.abs(cluster_data - mean_wave)
+        distance_threshold=2*std_wave
+        indices_to_keep=np.all(distances<=distance_threshold,axis=1)
+        filtered_cluster_data=cluster_data[indices_to_keep]
+        plotting_data=filtered_cluster_data.transpose()
+        firings[i]=len(filtered_cluster_data)*10000/len(data)
+        plt.subplot(3,1,i+1)
+        plt.plot(plotting_data,alpha=0.5)
+        plt.title(f'Cluster {i} \n numerosity: {len(filtered_cluster_data)}')
+        plt.xlabel('Time [ms]')
+        plt.ylabel('Signal Amplitude')
+        mean_wave = np.mean(filtered_cluster_data, axis=0)
+        std_wave = np.std(filtered_cluster_data, axis=0)
+        plt.errorbar(range(mean_wave.shape[0]), mean_wave, yerr=std_wave, color='black', linewidth=2, label='Avg. Waveform')
+        plt.legend(loc='lower right')
+        plt.show()
+        ul=spike_list[labels==i]
+        ull=ul[indices_to_keep]
+        final_data.append(ull)
+        plt.subplot(3, 1, i + 1)
+        plt.hist(np.diff(ull), bins=100, density=True, alpha=0.5, color='blue', edgecolor='black')
+        plt.title(f'ISI: Cluster {i}, \n firing rate: {format(len(final_data[i])*10000/len(data), ".2f")} Hz')
+        plt.show()
+    if len(unique_labels)>10:
+        fig = plt.figure(figsize=(18,8))
+        ax = fig.add_subplot(1, 2, 1, projection='3d')
+        ax.scatter(transformed[:,0], transformed[:,1], transformed[:,2], c=color, alpha=0.8, s=10, marker='.')
+        ax = fig.add_subplot(1, 2, 2)
+        for i,cluster_label in enumerate(unique_labels):    
+            idx=cluster_label==i
+            color = plt.rcParams['axes.prop_cycle'].by_key()['color'][i]
+            mean_wave = np.mean(cut[idx,:],axis = 0)
+            std_wave = np.std(cut[idx,:],axis = 0)
+            #ax.errorbar(range(cut[idx,:].shape[1]),mean_wave,yerr = std_wave)
+            plt.errorbar(range(mean_wave.shape[0]), mean_wave, yerr=std_wave, c=color)
+            #ax.errorbar(range(mean_wave.shape[0]),mean_wave,yerr = std_wave)
+
+        plt.xlabel('Time [0.1ms]')
+        plt.ylabel('Voltage [\u03BCV]')
+        plt.show()    
+
+    return final_data
+
 '''
